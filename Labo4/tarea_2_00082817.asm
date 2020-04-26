ab9af6769e9a9c5e439c221b415f982e875bb692
Fredy Sánchez <00082817@uca.edu.sv>
2020-04-25 22:37:27 -0600

    Ejercicio 2, terminado

--- Labo4/tarea_2_00082817.asm
+++ Labo4/tarea_2_00082817.asm
@@ -12,29 +12,55 @@
         div     cl		;(8+2+8+1+7)/5 = 5.2 => 5
 
 	mov     cl, "E"
-        mov [200h], cl
+        mov 	[200h], cl
         mov     cl, "N"
-        mov [201h], cl
+        mov 	[201h], cl
 
         mov     cl, "E"
-        mov [203h], cl
+        mov 	[203h], cl
 	mov     cl, "L"
-	mov [204h], cl
+	mov 	[204h], cl
 
 	mov     cl, "S"
-	mov [206h], cl
+	mov 	[206h], cl
 	mov     cl, "E"
-	mov [207h], cl
+	mov 	[207h], cl
 	mov     cl, "G"
-	mov [208h], cl
+	mov 	[208h], cl
 	mov     cl, "U"
-	mov [209h], cl
+	mov 	[209h], cl
 	mov     cl, "N"
-	mov [20Ah], cl
+	mov 	[20Ah], cl
 	mov     cl, "D"
-	mov [20Bh], cl
-	mov     cl, "0"
-	mov [20Ch], cl
+	mov 	[20Bh], cl
+	mov     cl, "O"
+	mov 	[20Ch], cl
+
+
+
+;EJERCICIO 2
+
+	mov 	ax, 0000h
+	mov     al, 2d
+	mov     bx, 210h
+	mov     cx, 2d
+
+calculo:mul     cx
+        mov     [bx], ax
+	
+	cmp     ah, 00h
+	ja      doscell
+
+	cmp	ah, 00h
+	je      unacell
+
+doscell:add     bx, 2h
+	jmp     dia
+
+unacell:add     bx, 1h
+
+dia:    cmp     bx, 21Fh
+	jb      calculo
 
 
 	int 20h
