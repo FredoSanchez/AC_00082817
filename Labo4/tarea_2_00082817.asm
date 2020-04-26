04c71e111bb24ccd6b1fd7cdf8763594d51db80d
Fredy Sánchez <00082817@uca.edu.sv>
2020-04-25 19:34:14 -0600

    Ejercicio 1, Mostrando mensaje desde 200h

--- Labo4/tarea_2_00082817.asm
+++ Labo4/tarea_2_00082817.asm
@@ -1,5 +1,7 @@
 	org     100h
 
+; EJERCICIO 1
+
 	mov     ax, 0000h
         add     ax, 8d
         add     ax, 2d
@@ -8,5 +10,29 @@
         add     ax, 7d
         mov     cl, 5d
         div     cl		;(8+2+8+1+7)/5 = 5.2 => 5
+
+	mov     cl, "E"
+        mov [200h], cl
+        mov     cl, "N"
+        mov [201h], cl
+        mov     cl, "E"
+        mov [202h], cl
+	mov     cl, "L"
+	mov [203h], cl
+	mov     cl, "S"
+	mov [204h], cl
+	mov     cl, "E"
+	mov [205h], cl
+	mov     cl, "G"
+	mov [206h], cl
+	mov     cl, "U"
+	mov [206h], cl
+	mov     cl, "N"
+	mov [208h], cl
+	mov     cl, "D"
+	mov [209h], cl
+	mov     cl, "0"
+	mov [20Ah], cl
+
 
 	int 20h
