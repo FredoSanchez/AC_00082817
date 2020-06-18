8298afb6f2cd04ee58a3c191888e6bb73675f6b0
Fredy Sánchez <00082817@uca.edu.sv>
2020-06-17 20:45:46 -0600

    Mensajes

---
+++ Tarea4/tarea_4_00082817.asm
@@ -0,0 +1,19 @@
+org 	100h
+
+section .text
+
+	
+section .data
+
+msg 	db 	"Ingrese los ultimos 5 digitos de su carnet: $"
+msg1 	db 	"Perfecto solo Dios$"
+msg2 	db 	"Siempre me esfuerzo$"
+msg3 	db 	"Colocho$"
+msg4 	db 	"Muy bien$"
+msg5 	db 	"Peor es nada$"
+msg6 	db 	"En el segundo$"
+msg7 	db 	"Me recupero$"
+msg8 	db 	"Hay salud$"
+msg9 	db 	"Aun se pasa$"
+msg10 	db 	"Solo necesito el 0$"
+nl	    db 	0xA, 0xD, "$"
