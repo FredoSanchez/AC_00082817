abe0dfd6e739bdbb3e29c35399c5ee047505916e
Fredy Sánchez <00082817@uca.edu.sv>
2020-06-24 20:09:11 -0600

    Tarea terminada

---
+++ Tarea5/tarea_5_00082817.asm
@@ -0,0 +1,138 @@
+org 	100h
+
+section .text
+
+	call 	grafico	; Llamada a iniciar modo grafico 13h
+
+	xor 	si, si
+	xor 	di, di
+
+	mov 	si, 115d ; X -> Columna
+	mov 	di, 25d ; Y -> Fila
+	call 	linea_h
+
+	mov 	si, 145d ; X -> Columna
+	mov 	di, 55d ; Y -> Fila
+	call 	linea_h
+
+	mov 	si, 145d ; X -> Columna
+	mov 	di, 85d ; Y -> Fila
+	call 	linea_h2
+
+	mov 	si, 145d ; X -> Columna
+	mov 	di, 115d ; Y -> Fila
+	call 	linea_h2
+
+	mov 	si, 115d ; X -> Columna
+	mov 	di, 175d ; Y -> Fila
+	call 	linea_h3
+
+	mov	si, 115d ; X -> Columna
+	mov 	di, 25d ; Y -> Fila
+	call 	linea_v
+
+	mov	si, 145d ; X -> Columna
+	mov 	di, 115d ; Y -> Fila
+	call 	linea_v
+
+	mov	si, 205d ; X -> Columna
+	mov 	di, 25d ; Y -> Fila
+	call 	linea_v2
+
+	mov	si, 145d ; X -> Columna
+	mov 	di, 55d ; Y -> Fila
+	call 	linea_v3
+
+	mov	si, 175d ; X -> Columna
+	mov 	di, 85d ; Y -> Fila
+	call 	linea_v4
+
+	call 	kb		; Utilizamos espera de alguna tecla
+
+	int 	20h
+
+grafico:mov	ah, 00h
+	mov	al, 13h
+	int 	10h
+	ret
+
+pixel:	mov	ah, 0Ch
+	mov	al, 1010b
+	int 	10h
+	ret
+
+linea_h: 
+lupi_h:	mov 	cx, 0d ; Columna 
+	add 	cx, si
+	mov	dx, di ; Fila
+	call 	pixel
+	inc 	si
+	cmp 	si, 205d
+	jne 	lupi_h
+	ret
+
+linea_h2: 
+lupi_h2:mov 	cx, 0d ; Columna 
+	add 	cx, si
+	mov	dx, di ; Fila
+	call 	pixel
+	inc 	si
+	cmp 	si, 175d
+	jne 	lupi_h2
+	ret
+
+linea_h3: 
+lupi_h3:mov 	cx, 0d ; Columna 
+	add 	cx, si
+	mov	dx, di ; Fila
+	call 	pixel
+	inc 	si
+	cmp 	si, 145d
+	jne 	lupi_h3
+	ret
+
+linea_v:
+lupi_v:	mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 175d
+	jne 	lupi_v
+	ret
+
+linea_v2:
+lupi_v2:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 55d
+	jne 	lupi_v2
+	ret
+
+linea_v3:
+lupi_v3:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 85d
+	jne 	lupi_v3
+	ret
+
+linea_v4:
+lupi_v4:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 115d
+	jne 	lupi_v4
+	ret
+
+kb: 	mov	ah, 00h
+	int 	16h
+	ret
+
+section .data
